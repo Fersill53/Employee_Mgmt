03b83cf84d87e031cbdebc1063cef290cb8f0350
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-08-22 22:25:44 -0500

    add employee.js

---
+++ employee.js
@@ -0,0 +1,38 @@
+const connection = require('./db');
+
+const getEmployee = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM employees', (err, results) => {
+            if (err) reject (err);
+            resolve(results);
+        });
+    });
+};
+
+const addEmployee = (employee) => {
+    return new Promise((reslove, reject) => {
+        connection.query('INSERT INTO employees SET ?', employee, (err, result) => {
+            if (err) reject (err);
+            resolve(result.insertID);
+        });
+    });
+};
+
+const updateEmployee = (id, role) => {
+    return new Promise((reslove, reject) => {
+        connection.query(
+            'UPDATE employees SET role = ? WHERE id = ?',
+            [role, id],
+            (err, result) => {
+                if (err) reject(err);
+                resolve(result.affectedRows > 0); 
+            }
+        );
+    });
+};
+
+module.exports = {
+    getEmployee,
+    addEmployee,
+    updateEmployee,
+};
